2c37069417c3d1176d714dcb7d51f4dbef7c6894
Zack Spalding <spalding@senylrc.org>
2021-10-25 14:15:09 -0400

    fixed spelling and added date

--- seal_script/seal_due_date.php
+++ seal_script/seal_due_date.php
@@ -3,51 +3,38 @@
 #####Connect to database
 require 'seal_db.inc';
 $db = mysqli_connect($dbhost, $dbuser, $dbpass);
-mysqli_select_db($db, $dbname);
+mysqli_select_db($db,$dbname);
 $today = date("Y-m-d");
 $fivedays = date('m/d/Y', strtotime('+5 days'));
 
 $sqlselect="SELECT * FROM `SENYLRC-SEAL2-STATS` WHERE `DueDate` = '".$fivedays."' and `receiveAccount` IS NOT NULL and `returnAccount` IS NULL AND `checkinAccount` IS NULL ORDER BY `index` ASC";
-$retval = mysqli_query($db, $sqlselect);
-$GETLISTCOUNT = mysqli_num_rows($retval);
-
+$retval = mysqli_query($db,$sqlselect);
+$GETLISTCOUNT = mysqli_num_rows ($retval);
 while ($row = mysqli_fetch_assoc($retval)) {
-    $timestamp      = $row["Timestamp"];
-    $destination = $row["Destination"];
-    $illnum = $row["illNUB"];
-    $title  = $row["Title"];
-    $author = $row["Author"];
-    $itype  = $row["Itype"];
-    $pubdate        = $row["pubdate"];
-    $isbn        = $row["reqisbn"];
-    $issn        = $row["reqissn"];
-    $article    = $row["article"];
-    $email  = $row["requesterEMAIL"];
+  $timestamp      = $row["Timestamp"];
+  $destination = $row["Destination"];
+  $illnum = $row["illNUB"];
+  $title  = $row["Title"];
+  $author = $row["Author"];
+  $itype  = $row["Itype"];
+  $pubdate        = $row["pubdate"];
+  $isbn        = $row["reqisbn"];
+  $issn        = $row["reqissn"];
+  $article    = $row["article"];
+  $email  = $row["requesterEMAIL"];
 
-    $duedate = $row["DueDate"];
+  $duedate = $row["DueDate"];
 
-    #########SETUP email
-    #Well set these to white space if they are empty to prevent an error message
-    if (empty($reqnote)) {
-        $reqnote='';
-    }
-    if (empty($isbn)) {
-        $isbn='';
-    }
-    if (empty($issn)) {
-        $issn='';
-    }
-    if (empty($itemcall)) {
-        $itemcall='';
-    }
-    if (empty($lname)) {
-        $lname='';
-    }
-    if (empty($arttile)) {
-        $article='';
-    }
+  #########SETUP email
+  #Well set these to white space if they are empty to prevent an error message
+  if ( empty($reqnote)) $reqnote='';
+  if ( empty($isbn)) $isbn='';
+  if ( empty($issn)) $issn='';
+  if ( empty($itemcall)) $itemcall='';
+  if ( empty($lname)) $lname='';
+  if ( empty($arttile)) $article='';
     ######Copy of message sent to the requester
-    $messagereq = "ILL# $illnum has a due date approacing: <br><br>
+    $messagereq = "ILL# $illnum has a due date approaching: $duedate <br><br>
       Title: $title <br>
       Author: $author<br>
       Item Type: $itype<br>
@@ -59,7 +46,7 @@ while ($row = mysqli_fetch_assoc($retval)) {
       <br>";
     #######Set email subject for request
     $subject = "SEAL Approching Due Date: for ILL# $illnum";
-    $subject = html_entity_decode($subject, ENT_QUOTES, 'UTF-8');
+    $subject = html_entity_decode (  $subject, ENT_QUOTES, 'UTF-8' );
     echo "$subject\n";
     #Set email to me for testing
     #$destemail = 'spalding@senylrc.org';
@@ -72,4 +59,8 @@ while ($row = mysqli_fetch_assoc($retval)) {
     $messagereq = preg_replace('/(?<!\r)\n/', "\r\n", $messagereq);
     $headers = preg_replace('/(?<!\r)\n/', "\r\n", $headers);
     mail($email, $subject, $messagereq, $headers, "-f ill@senylrc.org");
+
+
+
 }
+?>

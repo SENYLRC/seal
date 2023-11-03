2605c3ecc1183ffa65898748b3f9cd405c56f8a4
Zachary Spalding <spalding@senylrc.org>
2023-11-03 14:49:31 -0400

    update cron jobs

--- seal_script/seal_anonymizer.php
+++ seal_script/seal_anonymizer.php
@@ -1,6 +1,36 @@
 <?php
+// Connect to database
+require '/var/www/seal_script/seal_db.inc';
+$db = mysqli_connect($dbhost, $dbuser, $dbpass);
+mysqli_select_db($db, $dbname);
 
-$sqlupdate = "UPDATE `$sealSTAT` SET  `Title` = '', `Author` = '', `pubdate` = '', `reqisbn` = '', `reqissn` = '', `itype` = '', `Call Number` = '', `article` = '', `needbydate` = '', `patronnote` = '', `DueDate` = '', `emailsent` = '1' , `responderNOTE` =  '$respnote' WHERE `illNUB` = '$reqnumb'";
+$currentDate = new DateTime();
+
+// Add two years to the current date
+$currentDate->sub(new DateInterval('P3Y'));
+
+// Format the result as a string
+$ydate = $currentDate->format('Y-m-d');
+
+
+$testsqlselect ="select `Timestamp` from `$sealSTAT` WHERE `Timestamp` < '$ydate'";
+$sqlupdate = "UPDATE `$sealSTAT` SET  `Title` = '', `Author` = '', `pubdate` = '', `reqisbn` = '', `reqissn` = '', `itype` = '', `Call Number` = '', `article` = '', `needbydate` = '', `patronnote` = '', `DueDate` = '',  `reqNOTE` =  '' WHERE `Timestamp` < '$ydate'";
+
+//for test
+echo $testsqlselect."\n\n";
+echo $sqlupdate."\n\n";
+$result= mysqli_query($db, $sqlupdate);
+
+if ($result === false) {
+    // The query failed
+    echo "Update failed: " . mysqli_error($db);
+    // You can handle the failure in your preferred way (e.g., log the error, show an error message, etc.)
+} else {
+    // The query was successful
+    echo "Update succeeded!";
+    // You can perform additional actions after a successful query here.
+}
+
 
 
 ?>
